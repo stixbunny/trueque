e5d871fa01e60a530061d10fd0ed6f3635565e69
Milenko Castillo <milenko.castillo@gmail.com>
2018-04-23 01:07:23 -0300

    arreglado select

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,6 @@ class CommentsController < InheritedResources::Base
   private
 
     def comment_params
-      params.require(:comment).permit(:content, comment_items_attributes: [:product_id, :service_id, :name])
+      params.require(:comment).permit(:content, comment_item_attributes: [:product_id, :service_id, :name])
     end
 end
